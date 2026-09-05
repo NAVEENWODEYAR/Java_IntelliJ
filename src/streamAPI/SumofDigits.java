e30cec14ef1003f6e1c97cb71b5a868b252d65ea
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-09-05 22:07:24 +0530

    Update SumofDigits.java

--- src/streamAPI/SumofDigits.java
+++ src/streamAPI/SumofDigits.java
@@ -9,7 +9,7 @@ public class SumofDigits
 {
     public static void main(String[] args)
     {
-        System.out.println("Prints the sum of the digits in the list");
+        System.out.println("Prints the sum of the digits in the given list");
         List<Object> sum_OddNo = Collections.singletonList(List.of(1, 3, 5, 7, 9).stream().reduce((a, b) -> a + b));
         Optional<Integer> sum_OddNo1 = List.of(1, 3, 5, 7, 9).stream().reduce((a, b) -> a + b);
         System.out.println(sum_OddNo1.get());
